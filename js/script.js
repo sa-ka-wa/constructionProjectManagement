7829fe0dfbb8633e1e603f5374d8257fd88d3998
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-26 20:59:26 +0300

    outputs material inventory

--- js/script.js
+++ js/script.js
@@ -9,12 +9,15 @@ async function fetchActiveProjects() {
     <h2>${skyline.name}</h2>
     <p><strong>Status:</strong> ${skyline.status}</p>
     <p><strong>Completion:</strong> ${skyline.completionPercentage}%</p>
+  </div>
 `;
   const harbor = activeProject.harborViewComplex;
   document.getElementById("harbor-projects").innerHTML = `
+  <div class="project-list" onclick="showProjectDetails(${harbor.id})">
     <h2>${harbor.name}</h2>
     <p><strong>Status:</strong> ${harbor.status}</p>
     <p><strong>Completion:</strong> ${harbor.completionPercentage}%</p>
+  </div>
 `;
   window.projectData = {
     [activeProject.skylineTowers.id]: activeProject.skylineTowers,
@@ -25,9 +28,25 @@ fetchActiveProjects();
 
 function showProjectDetails(projectId) {
   const project = window.projectData[projectId];
-
-  if (project) {
-    document.getElementById("inventory").innerHTML = `
+  const renderMaterial = (project) => {
+    const materialList = document.getElementById("material-list");
+    materialList.innerHTML = "";
+    Object.values(project.materialsInventory).forEach((material) => {
+      materialList.innerHTML += `<tr>
+          <td>${material.materialName}</td>
+          <td>${material.quantity}</td>
+          <td>${material.unit}</td>
+          <td>${material.status}</td>
+  </tr>`;
+      console.log("Rendering materials for project:", project);
+    });
+  };
+  renderMaterial(project);
+  function projectDetails(projectId) {
+    const projectDetail = window.projectData[projectId];
+    if (project) {
+      document.getElementById("recent-task").innerHTML = `
+       <div>
       <h1>${project.name} - Details</h1>
       <h2>Recent Tasks</h2>
       <ul>
@@ -38,21 +57,7 @@ function showProjectDetails(projectId) {
         `
           )
           .join("")}
-      </ul>`;
+      </ul></div>`;
+    }
   }
-}
-
-function projectDetails(projectId) {
-  const projectDetail = window.projectData[projectId];
-  return Object.entries(projectDetail.materialsInventory)
-    .map(
-      ([key, material]) => `
-        <tr>
-            <td>${key}</td>
-            <td>${material.quantity}</td>
-            <td>${material.unit}</td>
-            <td>${material.status}</td>
-        </tr>`
-    )
-    .join("");
 }
