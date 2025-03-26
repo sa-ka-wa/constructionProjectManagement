cb9d1f4be78d686ed2457a3ec07ddf0d0afe41bc
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-26 13:10:28 +0300

    information outputs in html live server

--- js/script.js
+++ js/script.js
@@ -2,5 +2,18 @@ async function fetchActiveProjects() {
   const project = await fetch("http://localhost:3000/activeProject");
   const activeProject = await project.json();
   console.log("Active Projects Data:", activeProject);
+
+  const skyline = activeProject.skylineTowers;
+  document.getElementById("skyline-projects").innerHTML = `
+    <h2>${skyline.name}</h2>
+    <p><strong>Status:</strong> ${skyline.status}</p>
+    <p><strong>Completion:</strong> ${skyline.completionPercentage}%</p>
+`;
+  const harbor = activeProject.harborViewComplex;
+  document.getElementById("harbor-projects").innerHTML = `
+    <h2>${harbor.name}</h2>
+    <p><strong>Status:</strong> ${harbor.status}</p>
+    <p><strong>Completion:</strong> ${harbor.completionPercentage}%</p>
+`;
 }
 fetchActiveProjects();
